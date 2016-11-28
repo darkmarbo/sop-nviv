78de24d337d8fa4008456b2214740e4fc615ac1a
darkmarbo <darkmarbo@163.com>
2016-11-28 16:15:28 +0800

    v

--- format_vivn.py
+++ format_vivn.py
@@ -16,6 +16,8 @@ f_line1=sys.argv[1]
 f_line2=sys.argv[2]
 f_out=sys.argv[3]
 
+ch_punc=['.','?','!',',',';',':','——','\'','"','[',']','(',')','’','“','”'];
+
 ### 记录 序号+line
 map_num={};
 ### 记录 词性集
@@ -60,6 +62,7 @@ for line in fp_line1:
 
 #print("map_num len=%d"%(len(map_num.keys())));
 
+#### 处理每一行  
 ii=0
 for line in fp_line2:
 
@@ -85,16 +88,46 @@ for line in fp_line2:
     ### 输出 句子编号+原始句子 
     fp_out.write("%s\t%s\n"%(num, line_ori));
 
+    #### 原始句子行  标点符合分开 前后都加空格  然后删除连续两个空格 
+    line_ori_find=line_ori;
+    for ch in ch_punc:
+        line_ori_find = line_ori_find.replace(ch," %s "%(ch));
+
+    line_ori_find = line_ori_find.replace("   "," ");
+    line_ori_find = line_ori_find.replace("  "," ");
+    line_ori_find = line_ori_find.strip();
+    vec_word_ori = line_ori_find.split(" ");
+
+    n_vec_ori = len(vec_word_ori);  ### 原始句子的词数 
+    
+    #print("%s\n%s"%(line_ori, line_ori_find));
+
+
     #### 每一个word/pos
     list_cont = content.split(' ')
         
+    jj_st = 0;  ### 从 line_ori_find 的 jj_st 开始查找 
+    jj_end = 0;  ###    line_ori_find 的 jj_end 结束  
+    len_1 = len(line_ori_find);  ### 原始句子长度 
+    len_2 = 0;  ### 分词结果 累计长度和 
+    len_diff = 4;   ### 从jj_st到现在相差多少  
+    N_list_cont = len(list_cont);
+    n_list_cont = 0;  ### 第几个 www 
     for word in list_cont:
 
+        n_list_cont += 1;
         list_pos = word.split('/');
 
         #### 得到词和词性 
         www = list_pos[0]
         www = www.replace('_', ' ');
+
+        ### 把 www  左右两侧的空格也加上来  全词匹配  
+        www = www.strip();
+        if n_list_cont != 1:
+            www = " %s"%(www)
+        if n_list_cont != N_list_cont:
+            www = "%s "%(www)
 
         pos = ""
         if len(list_pos)<2:
@@ -106,11 +139,26 @@ for line in fp_line2:
         if not map_pos.has_key(pos):
             print("ERROR:pos-tagger error!num=%d\tword=%s\tpos=%s\n"%(ii, www, pos));
 
-        #### 词错误  去原始文本中查找分词后的词  
-        www_find = line_ori.find(www);
-        if www_find<0:
+        len_www = len(www);  ### 当前词的长度 
+        jj_end = jj_st + len_www + len_diff;
+        if jj_end > len_1:
+            jj_end = len_1;
+            
+        #### 查找 www  从jj_st索引  到jj_end(不包含)
+        ##### 词错误  去原始文本中查找分词后的词  
+        #if ii == 392:
+        #    print "%s"%(line_ori)
+        #    print "www=%s==="%(www)
+        idx = line_ori_find.find(www, jj_st, jj_end); 
+        if idx < 0:
+            len_diff += len_www + 4;
             print("ERROR:word_segment error!num=%d\tline_ori=%s\terr_word=%s\t\n"%\
                         (ii, line_ori, www));
+        else:
+            jj_st = idx + len_www -2;
+            len_diff = 4;
+        
+
 
         fp_out.write("%s\t%s\n"%(www, pos))
 
